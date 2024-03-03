9015a696f8191e7d2b2f8566e44a5478a8c7c200
David Czuchnowski <david.czuchnowski@davdev.pl>
2024-03-03 13:05:36 +0100

    loading env file, first impl

--- lib/features/dashboard/view/dashboard_page.dart
+++ lib/features/dashboard/view/dashboard_page.dart
@@ -1,3 +1,6 @@
+import 'dart:developer';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:macos_envvar/features/dashboard/dashboard.dart';
@@ -15,52 +18,66 @@ class DashboardPage extends StatelessWidget {
   }
 }
 
-class DashboardView extends StatelessWidget {
+class DashboardView extends StatefulWidget {
   const DashboardView({super.key});
+
+  @override
+  State<DashboardView> createState() => _DashboardViewState();
+}
+
+class _DashboardViewState extends State<DashboardView> {
+  String? zshrcContent;
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     final l10n = context.l10n;
     return Scaffold(
       appBar: AppBar(title: Text(l10n.dashboardAppBarTitle)),
-      body: const Center(child: CounterText()),
-      floatingActionButton: _buildActionButton(context),
+      body: _buildContent(),
+      floatingActionButton: _buildActionButton(context, l10n),
     );
   }
 
-  Column _buildActionButton(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.end,
-      crossAxisAlignment: CrossAxisAlignment.end,
-      children: [
-        FloatingActionButton(
-          onPressed: () => context.read<DashboardCubit>().increment(),
-          child: const Icon(Icons.add),
-        ),
-        const SizedBox(height: 8),
-        FloatingActionButton(
-          onPressed: () => context.read<DashboardCubit>().decrement(),
-          child: const Icon(Icons.remove),
-        ),
-      ],
+  Widget _buildActionButton(BuildContext context, AppLocalizations l10n) {
+    return FloatingActionButton(
+      onPressed: _readZshrc,
+      child: const Icon(Icons.add),
     );
   }
-}
 
-class CounterText extends StatelessWidget {
-  const CounterText({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        OutlinedButton(
-          onPressed: () {},
-          child: Text(
-            context.l10n.loadButtonText,
-          ),
-        ),
-      ],
+  Widget _buildContent() {
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('Welcome to the Dashboard'),
+          const SizedBox(height: 16),
+          _buildZshrcContent(),
+        ],
+      ),
     );
+  }
+
+  Widget _buildZshrcContent() {
+    if (zshrcContent?.isEmpty ?? true) {
+      return const Text('No .zshrc file found');
+    }
+    return Text(zshrcContent!);
+  }
+
+  Future<void> _readZshrc() async {
+    final file = File('/Users/david/.zshrc');
+    if (file.existsSync()) {
+      zshrcContent = await file.readAsString();
+
+      setState(() {});
+    } else {
+      log('No .zshrc file found');
+    }
   }
 }
