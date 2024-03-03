c392919f14d49af5de9e78de2f264460ccaeea11
David Czuchnowski <david.czuchnowski@davdev.pl>
2024-03-03 12:07:48 +0100

    Added load env button

--- lib/features/dashboard/view/dashboard_page.dart
+++ lib/features/dashboard/view/dashboard_page.dart
@@ -22,23 +22,27 @@ class DashboardView extends StatelessWidget {
   Widget build(BuildContext context) {
     final l10n = context.l10n;
     return Scaffold(
-      appBar: AppBar(title: Text(l10n.counterAppBarTitle)),
+      appBar: AppBar(title: Text(l10n.dashboardAppBarTitle)),
       body: const Center(child: CounterText()),
-      floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          FloatingActionButton(
-            onPressed: () => context.read<DashboardCubit>().increment(),
-            child: const Icon(Icons.add),
-          ),
-          const SizedBox(height: 8),
-          FloatingActionButton(
-            onPressed: () => context.read<DashboardCubit>().decrement(),
-            child: const Icon(Icons.remove),
-          ),
-        ],
-      ),
+      floatingActionButton: _buildActionButton(context),
+    );
+  }
+
+  Column _buildActionButton(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        FloatingActionButton(
+          onPressed: () => context.read<DashboardCubit>().increment(),
+          child: const Icon(Icons.add),
+        ),
+        const SizedBox(height: 8),
+        FloatingActionButton(
+          onPressed: () => context.read<DashboardCubit>().decrement(),
+          child: const Icon(Icons.remove),
+        ),
+      ],
     );
   }
 }
@@ -48,8 +52,15 @@ class CounterText extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    final count = context.select((DashboardCubit cubit) => cubit.state);
-    return Text('$count', style: theme.textTheme.displayLarge);
+    return Column(
+      children: [
+        OutlinedButton(
+          onPressed: () {},
+          child: Text(
+            context.l10n.loadButtonText,
+          ),
+        ),
+      ],
+    );
   }
 }
