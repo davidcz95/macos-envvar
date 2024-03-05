e51fa4a4f8fa63de9d75667f327a5d40d42a3720
David Czuchnowski <david.czuchnowski@davdev.pl>
2024-03-05 09:38:53 +0100

    Cleanup

--- lib/features/dashboard/view/dashboard_page.dart
+++ lib/features/dashboard/view/dashboard_page.dart
@@ -72,15 +72,20 @@ class _DashboardViewState extends State<DashboardView> {
       itemBuilder: (context, index) {
         final key = zshrcContent!.keys.elementAt(index);
         final value = zshrcContent![key];
-        return InkWell(
-          onTap: () {
-            log('Tapped on $key');
-          },
-          child: Card(
-            child: ListTile(
-              title: Text(key),
-              subtitle: Text(value.toString()),
-            ),
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 20),
+          child: CustomExpansionTileScale(
+            title: Text(key),
+            // subtitle: Text('Value: ${value!.values.first}'),
+            children: [
+              ...value?.entries
+                      .map(
+                        (entry) =>
+                            Text('Order: ${entry.key}, Value: ${entry.value}'),
+                      )
+                      .toList() ??
+                  [],
+            ],
           ),
         );
       },
@@ -127,5 +132,264 @@ class _DashboardViewState extends State<DashboardView> {
 
     return envVars;
   }
+}
 
+class CustomExpansionTile extends StatefulWidget {
+  const CustomExpansionTile({
+    required this.title,
+    required this.children,
+    super.key,
+    this.animationDuration = const Duration(milliseconds: 200),
+    this.curve = Curves.fastOutSlowIn,
+    this.trailing,
+    this.subtitle,
+  });
+
+  final Widget title;
+  final Widget? subtitle;
+  final Widget? trailing;
+  final List<Widget> children;
+  final Duration animationDuration;
+  final Curve curve;
+
+  @override
+  State<CustomExpansionTile> createState() => _CustomExpansionTileState();
+}
+
+class _CustomExpansionTileState extends State<CustomExpansionTile>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _heightFactor;
+
+  bool _isExpanded = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController =
+        AnimationController(duration: widget.animationDuration, vsync: this);
+    _heightFactor = _animationController.drive(CurveTween(curve: widget.curve));
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  void _toggleExpansion() {
+    setState(() {
+      _isExpanded = !_isExpanded;
+    });
+    if (_isExpanded) {
+      _animationController.forward();
+    } else {
+      _animationController.reverse();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: _toggleExpansion,
+      borderRadius: BorderRadius.circular(20),
+      child: Ink(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Colors.grey[800]!, // Darker color
+              Theme.of(context).cardColor, // Lighter color
+              Colors.grey[800]!, // Darker color
+            ],
+          ),
+          boxShadow: const [
+            BoxShadow(
+              offset: Offset(0, 10),
+              blurRadius: 30,
+            ),
+          ],
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(5),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              ListTile(
+                title: widget.title,
+                subtitle: widget.subtitle,
+                trailing: widget.trailing,
+              ),
+              AnimatedBuilder(
+                animation: _heightFactor,
+                builder: (BuildContext context, Widget? child) {
+                  return ClipRect(
+                    child: Align(
+                      heightFactor: _heightFactor.value,
+                      child: child,
+                    ),
+                  );
+                },
+                child: Column(children: widget.children),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomExpansionTileScale extends StatefulWidget {
+  const CustomExpansionTileScale({
+    required this.title,
+    required this.children,
+    super.key,
+    this.animationDuration = const Duration(milliseconds: 200),
+    this.curve = Curves.fastOutSlowIn,
+    this.trailing,
+    this.subtitle,
+  });
+
+  final Widget title;
+  final Widget? subtitle;
+  final Widget? trailing;
+  final List<Widget> children;
+  final Duration animationDuration;
+  final Curve curve;
+
+  @override
+  State<CustomExpansionTileScale> createState() =>
+      _CustomExpansionTileScaleState();
+}
+
+class _CustomExpansionTileScaleState extends State<CustomExpansionTileScale>
+    with TickerProviderStateMixin {
+  late AnimationController _animationController;
+  late AnimationController _hoverAnimationController;
+  late Animation<double> _heightFactor;
+  late Animation<double> _scaleAnimation;
+  bool _isExpanded = false;
+  bool _isHovering = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController =
+        AnimationController(duration: widget.animationDuration, vsync: this);
+    _heightFactor = _animationController.drive(CurveTween(curve: widget.curve));
+
+    _hoverAnimationController = AnimationController(
+      duration: const Duration(milliseconds: 150),
+      vsync: this,
+    );
+    _scaleAnimation =
+        Tween(begin: 1.0, end: 1.035).animate(_hoverAnimationController);
+
+    _hoverAnimationController.addListener(() {
+      if (!_isHovering &&
+          _hoverAnimationController.value ==
+              _hoverAnimationController.upperBound) {
+        _hoverAnimationController.reverse();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    _hoverAnimationController.dispose();
+    super.dispose();
+  }
+
+  void _toggleExpansion() {
+    setState(() {
+      _isExpanded = !_isExpanded;
+    });
+    if (_isExpanded) {
+      _animationController.forward();
+    } else {
+      _animationController.reverse();
+    }
+  }
+
+  void _onHover(bool isHovering) {
+    setState(() {
+      _isHovering = isHovering;
+    });
+    if (isHovering) {
+      _hoverAnimationController.forward();
+    } else {
+      _hoverAnimationController.reverse();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final decoration = BoxDecoration(
+      borderRadius: BorderRadius.circular(20),
+      gradient: LinearGradient(
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+        colors: [
+          Colors.amber[800]!, // Darker color
+          Theme.of(context).cardColor, // Lighter color
+          Colors.amber[800]!, // Darker color
+        ],
+      ),
+      boxShadow: const [
+        BoxShadow(
+          offset: Offset(0, 10),
+          blurRadius: 30,
+        ),
+      ],
+    );
+
+    return MouseRegion(
+      onEnter: (_) => _onHover(true),
+      onExit: (_) => _onHover(false),
+      child: AnimatedBuilder(
+        animation: Listenable.merge([_scaleAnimation, _heightFactor]),
+        builder: (context, child) {
+          return Transform.scale(
+            scale: _scaleAnimation.value,
+            child: Container(
+              decoration: decoration,
+              child: InkWell(
+                onTap: _toggleExpansion,
+                onHover: _onHover,
+                borderRadius: BorderRadius.circular(20),
+                child: Padding(
+                  padding: const EdgeInsets.all(5),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      ListTile(
+                        title: widget.title,
+                        subtitle: widget.subtitle,
+                        trailing: widget.trailing,
+                      ),
+                      ClipRect(
+                        child: Align(
+                          heightFactor: _heightFactor.value,
+                          child: child,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: widget.children,
+        ),
+      ),
+    );
+  }
 }
