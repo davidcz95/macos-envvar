6eea4538a407a35ad92cd1e204f806d1a4ce0540
David Czuchnowski <david.czuchnowski@davdev.pl>
2024-03-09 12:10:00 +0100

    Add model for parsed env file

---
+++ lib/data/models/env_file.dart
@@ -0,0 +1,38 @@
+/// Parsed .zshrc file content with freezed
+class ZshrcContent {
+  const ZshrcContent({required this.content});
+
+  factory ZshrcContent.fromJson(Map<String, Object?> json) {
+    return ZshrcContent(
+      content: (json['content']! as Map<String, Object?>).map(
+        (k, e) => MapEntry(
+          k,
+          (e! as Map<String, Object?>).map(
+            (k, e) => MapEntry(
+              int.parse(k),
+              e! as String,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  final Map<String, Map<int, String>> content;
+
+  Map<String, Object?> toJson() {
+    return {
+      'content': content.map(
+        (k, e) => MapEntry(
+          k,
+          e.map(
+            (k, e) => MapEntry(
+              k.toString(),
+              e,
+            ),
+          ),
+        ),
+      ),
+    };
+  }
+}
